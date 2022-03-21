0b6da58b2ce29863345e3b55b7cace38fcb2dcff
Marina Garceau <marina.garceau@du.edu>
2022-03-21 14:08:49 +0100

    initial version of square numbers not including n

---
+++ square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 13:29:50 2022
+
+@author: marcimac
+"""
+
+def square(n):
+    return[i**2 for i in range(n)]
+print(square(10))
